9d52d5203a53b33f7f1d48aa2deb703a85421332
smacneil1 <smacneil88@gmail.com>
2016-02-22 14:54:48 -0700

    MCL-1 update
    Added new plots for manuscript

--- Mcl1Dependence_Xiao.R
+++ Mcl1Dependence_Xiao.R
@@ -5,16 +5,26 @@ source("http://www.r-statistics.com/wp-content/uploads/2010/02/Barnard.R.txt")
 source("http://www.r-statistics.com/wp-content/uploads/2012/01/source_https.r.txt")
 source_https("https://raw.github.com/talgalili/R-code-snippets/master/Barnard.R")
 MCLdata=read.csv("BH3_Profiling/Mcl1_Xiao_Data.txt",header = T,sep='\t')[1:23,1:5]
+MCLdata_ccle=read.csv("BH3_Profiling/Mcl1_Xiao_Data_ccle.txt",header = T,sep='\t')[1:30,1:5]
+
+
 
 View(MCLdata)
 rownames(MCLdata)
 
 MCLdata[1:13,4]
 
+pdf("~/Documents/Multipathway_Modeling/BH3_Profiling/Mcl1_Plots.pdf")
 
-boxplot(MCLdata[1:12,4],MCLdata[13:23,4], ylab="% Viability upon treatment (MCL-1 siRNA)",names = c("AKT Phenotype \n n=14", " EGFR Phenotype \n n=9"), col = c("red", "grey"), main="Mcl-1 Dependence Across Growth Phenotypes", ylim=c(0,100))
+boxplot(MCLdata_ccle[1:19,4],MCLdata_ccle[20:30,4], ylab="% Viability upon treatment (MCL-1 siRNA) CCLE",names = c("AKT Phenotype \n n=19", " EGFR Phenotype \n n=11"), col = c("red", "grey"), main="Mcl-1 Dependence Across Growth Phenotypes", ylim=c(0,100))
+text(1,2,"p-value = 0.32")
+t.test(MCLdata_ccle[1:19,4],MCLdata_ccle[20:30,4])
+
+
+boxplot(MCLdata[1:12,4],MCLdata[13:23,4], ylab="% Viability upon treatment (MCL-1 siRNA)",names = c("AKT Phenotype \n n=12", " EGFR Phenotype \n n=10"), col = c("red", "grey"), main="Mcl-1 Dependence Across Growth Phenotypes", ylim=c(0,100))
 text(1,2,"p-value = 0.43")
 t.test(MCLdata[1:14,4],MCLdata[15:23,4])
+dev.off()
 
 
 
